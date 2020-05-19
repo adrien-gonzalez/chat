2276d2de7f9c8e54d57ec3c0a1a4031b0de3b5eb
sarah <sarah.chaouati@laplateforme.io>
2020-05-19 16:15:25 +0200

    delete.php modif pour delete msg user en meme temps que delete user :)

--- sources/delete.php
+++ sources/delete.php
@@ -3,6 +3,7 @@
 
 	$id = $_POST['id'];
 
-	$select ="DELETE FROM user WHERE id =  '$id' ";
+	$select="DELETE user, message  FROM user INNER JOIN message ON
+    		  user.id= message.id_user WHERE user.id=  $id";
 	$query=mysqli_query($base, $select) or die ($select);
 ?>
